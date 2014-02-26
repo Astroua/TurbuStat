edaf371ef8e6a477569dfffb274955e6443c5377
e-koch <koch.eric.w@gmail.com>
2014-02-25 21:13:10 -0800

    Added bash script to run all cases

---
+++ exec_allruns.sh
@@ -0,0 +1,53 @@
+#python output.py Fiducial128_1.0.0 0 distances_fiducial1.0 T 10
+#cd /srv/astro/erickoch/Dropbox/code_development/TurbuStat/TurbuStat
+#python output.py Fiducial128_1.0.0 2 distances_fiducial1.0 T 10
+#cd /srv/astro/erickoch/Dropbox/code_development/TurbuStat/TurbuStat
+#python output.py Fiducial128_1.2.0 0 distances_fiducial1.2 T 10
+#cd /srv/astro/erickoch/Dropbox/code_development/TurbuStat/TurbuStat
+#python output.py Fiducial128_1.2.0 2 distances_fiducial1.2 T 10
+#cd /srv/astro/erickoch/Dropbox/code_development/TurbuStat/TurbuStat
+### Fid 2
+#python output.py Fiducial128_2.0.0 0 distances_fiducial2.0 T 10
+#cd /srv/astro/erickoch/Dropbox/code_development/TurbuStat/TurbuStat
+#python output.py Fiducial128_2.0.0 2 distances_fiducial2.0 T 10
+#cd /srv/astro/erickoch/Dropbox/code_development/TurbuStat/TurbuStat
+#python output.py Fiducial128_2.2.0 0 distances_fiducial2.2 T 10
+#cd /srv/astro/erickoch/Dropbox/code_development/TurbuStat/TurbuStat
+#python output.py Fiducial128_2.2.0 2 distances_fiducial2.2 T 10
+#cd /srv/astro/erickoch/Dropbox/code_development/TurbuStat/TurbuStat
+### Fid 3
+#python output.py Fiducial128_3.0.0 0 distances_fiducial3.0 T 10
+#cd /srv/astro/erickoch/Dropbox/code_development/TurbuStat/TurbuStat
+#python output.py Fiducial128_3.0.0 2 distances_fiducial3.0 T 10
+#cd /srv/astro/erickoch/Dropbox/code_development/TurbuStat/TurbuStat
+#python output.py Fiducial128_3.2.0 0 distances_fiducial3.2 T 10
+#cd /srv/astro/erickoch/Dropbox/code_development/TurbuStat/TurbuStat
+#python output.py Fiducial128_3.2.0 2 distances_fiducial3.2 T 10
+#cd /srv/astro/erickoch/Dropbox/code_development/TurbuStat/TurbuStat
+### FID 4
+#python output.py Fiducial128_4.0.0 0 distances_fiducial4.0 T 10
+#cd /srv/astro/erickoch/Dropbox/code_development/TurbuStat/TurbuStat
+#python output.py Fiducial128_4.0.0 2 distances_fiducial4.0 T 10
+#cd /srv/astro/erickoch/Dropbox/code_development/TurbuStat/TurbuStat
+#python output.py Fiducial128_4.2.0 0 distances_fiducial4.2 T 10
+#cd /srv/astro/erickoch/Dropbox/code_development/TurbuStat/TurbuStat
+#python output.py Fiducial128_4.2.0 2 distances_fiducial4.2 T 10
+#cd /srv/astro/erickoch/Dropbox/code_development/TurbuStat/TurbuStat
+## FID 5
+python output.py Fiducial128_5.0.0 0 distances_fiducial5.0 T 10
+cd /srv/astro/erickoch/Dropbox/code_development/TurbuStat/TurbuStat
+python output.py Fiducial128_5.0.0 2 distances_fiducial5.0 T 10
+cd /srv/astro/erickoch/Dropbox/code_development/TurbuStat/TurbuStat
+python output.py Fiducial128_5.2.0 0 distances_fiducial5.2 T 10
+cd /srv/astro/erickoch/Dropbox/code_development/TurbuStat/TurbuStat
+python output.py Fiducial128_5.2.0 2 distances_fiducial5.2 T 10
+cd /srv/astro/erickoch/Dropbox/code_development/TurbuStat/TurbuStat
+## FID 6
+python output.py Fiducial128_6.0.0 0 distances_fiducial6.0 T 10
+cd /srv/astro/erickoch/Dropbox/code_development/TurbuStat/TurbuStat
+python output.py Fiducial128_6.0.0 2 distances_fiducial6.0 T 10
+cd /srv/astro/erickoch/Dropbox/code_development/TurbuStat/TurbuStat
+python output.py Fiducial128_6.2.0 0 distances_fiducial6.2 T 10
+cd /srv/astro/erickoch/Dropbox/code_development/TurbuStat/TurbuStat
+python output.py Fiducial128_6.2.0 2 distances_fiducial6.2 T 10
+cd /srv/astro/erickoch/Dropbox/code_development/TurbuStat/TurbuStat
