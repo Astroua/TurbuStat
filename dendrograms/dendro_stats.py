55ba5775c3f06db262365785b35525043c160b17
e-koch <koch.eric.w@gmail.com>
2014-05-13 12:46:31 -0700

    Linear model accepts different delta for the two data sets.

--- dendrograms/dendro_stats.py
+++ dendrograms/dendro_stats.py
@@ -41,7 +41,8 @@ class DendroDistance(object):
         self.file2 = self.file2[timestep]
 
         assert (self.file1["Deltas"] == self.file2["Deltas"]).all()
-        self.deltas = self.file1["Deltas"]
+        self.deltas1 = self.file1["Deltas"]
+        self.deltas2 = self.file2["Deltas"]
 
         ## Set the minimum number of components to create a histogram
         self.cutoff = min(np.argwhere((self.file1["Num Features"]<min_features)==1)[0], \
@@ -64,30 +65,23 @@ class DendroDistance(object):
         '''
         '''
 
-        self.numdata1 = np.log10(self.file1["Num Features"].ix[np.where(np.log10(self.deltas)>-0.6)])
-        self.numdata2 = np.log10(self.file2["Num Features"].ix[np.where(np.log10(self.deltas)>-0.6)])
-        clip_delta = self.deltas.ix[np.where(np.log10(self.deltas)>-0.6)]
+        self.numdata1 = np.log10(self.file1["Num Features"].ix[np.where(np.log10(self.deltas1)>-0.6)])
+        self.numdata2 = np.log10(self.file2["Num Features"].ix[np.where(np.log10(self.deltas2)>-0.6)])
+        clip_delta1 = self.deltas1.ix[np.where(np.log10(self.deltas1)>-0.6)]
+        clip_delta2 = self.deltas2.ix[np.where(np.log10(self.deltas2)>-0.6)]
 
         if (self.numdata1==0).any() or (self.numdata2==0).any():
-            filter11 = self.numdata1[self.numdata1>0]
-            filter21 = self.numdata2[self.numdata1>0]
-            filter12 = filter11[self.numdata2>0]
-            filter22 = filter21[self.numdata2>0]
+            self.numdata1 = self.numdata1[self.numdata1>0]
+            self.numdata2 = self.numdata2[self.numdata2>0]
 
-            clip_delta = clip_delta[self.numdata1>0]
-            clip_delta = clip_delta[self.numdata2>0]
-
-            if filter12.empty or filter22.empty: ## If we can't compare at all, just exit.
-                self.num_distance = 0.0
-                return self
-            self.numdata1 = filter12
-            self.numdata2 = filter22
+            clip_delta1 = clip_delta1[self.numdata1>0]
+            clip_delta2 = clip_delt2[self.numdata2>0]
 
         ## Set up columns for regression
-        dummy = [0] * len(clip_delta) + [1] * len(clip_delta)
-        x = np.concatenate((np.log10(clip_delta), np.log10(clip_delta)))
+        dummy = [0] * len(clip_delta1) + [1] * len(clip_delta2)
+        x = np.concatenate((np.log10(clip_delta1), np.log10(clip_delta2)))
         regressor = x.T * dummy
-        constant = np.array([[1] * (len(clip_delta) + len(clip_delta))])
+        constant = np.array([[1] * (len(clip_delta1) + len(clip_delta2))])
 
         numdata = np.concatenate((np.log10(self.numdata1), np.log10(self.numdata2)))
 
@@ -106,8 +100,8 @@ class DendroDistance(object):
             print self.num_results.summary()
 
             import matplotlib.pyplot as p
-            p.plot(np.log10(clip_delta), np.log10(self.numdata1), "bD", \
-                    np.log10(clip_delta), np.log10(self.numdata2), "gD")
+            p.plot(np.log10(clip_delta1), np.log10(self.numdata1), "bD", \
+                    np.log10(clip_delta2), np.log10(self.numdata2), "gD")
             p.plot(df["scales"][:len(self.numdata1)], self.num_results.fittedvalues[:len(self.numdata1)], "b", \
                    df["scales"][-len(self.numdata2):], self.num_results.fittedvalues[-len(self.numdata2):], "g")
             p.grid(True)
@@ -125,12 +119,12 @@ class DendroDistance(object):
             self.nbins = [int(round(np.sqrt((n1+n2)/2.))) for n1, n2 in zip(self.file1["Num Features"].ix[:self.cutoff], \
                             self.file2["Num Features"].ix[:self.cutoff])]
         else:
-            self.nbins = [self.nbins] * len(self.deltas.ix[:self.cutoff])
+            self.nbins = [self.nbins] * len(self.deltas1.ix[:self.cutoff])
 
-        self.histograms1 = np.empty((len(self.deltas.ix[:self.cutoff]), np.max(self.nbins)))
-        self.histograms2 = np.empty((len(self.deltas.ix[:self.cutoff]), np.max(self.nbins)))
+        self.histograms1 = np.empty((len(self.deltas1.ix[:self.cutoff]), np.max(self.nbins)))
+        self.histograms2 = np.empty((len(self.deltas2.ix[:self.cutoff]), np.max(self.nbins)))
 
-        for n, (data1,data2,nbin) in enumerate(zip(self.file1["Histograms"].ix[:self.cutoff], \
+        for n, (data1,data2,nbin) in enumerate(zip(self.file1["Histograms"].ix[:self.cutoff],
                                       self.file2["Histograms"].ix[:self.cutoff], self.nbins)):
             hist1, bins = np.histogram(data1, bins=nbin, density=True, range=[0,1])[:2]
             self.histograms1[n,:] = np.append(hist1, (np.max(self.nbins) - nbin) * [np.NaN])
@@ -153,22 +147,22 @@ class DendroDistance(object):
             p.subplot(2,2,1)
             p.title("ECDF 1")
             p.xlabel("Intensities")
-            for n in range(len(self.deltas[:self.cutoff])):
+            for n in range(len(self.deltas1[:self.cutoff])):
                 p.plot((self.bins[n][:-1] + self.bins[n][1:])/2, self.mecdf1[n,:][:self.nbins[n]])
             p.subplot(2,2,2)
             p.title("ECDF 2")
             p.xlabel("Intensities")
-            for n in range(len(self.deltas[:self.cutoff])):
+            for n in range(len(self.deltas2[:self.cutoff])):
                 p.plot((self.bins[n][:-1] + self.bins[n][1:])/2, self.mecdf2[n,:][:self.nbins[n]])
             p.subplot(2,2,3)
             p.title("PDF 1")
-            for n in range(len(self.deltas[:self.cutoff])):
+            for n in range(len(self.deltas1[:self.cutoff])):
                 bin_width = self.bins[n][1]-self.bins[n][0]
                 p.bar((self.bins[n][:-1] + self.bins[n][1:])/2, self.histograms1[n,:][:self.nbins[n]], \
                     align="center", width=bin_width, alpha=0.25)
             p.subplot(2,2,4)
             p.title("PDF 2")
-            for n in range(len(self.deltas[:self.cutoff])):
+            for n in range(len(self.deltas2[:self.cutoff])):
                 bin_width = self.bins[n][1]-self.bins[n][0]
                 p.bar((self.bins[n][:-1] + self.bins[n][1:])/2, self.histograms2[n,:][:self.nbins[n]], \
                     align="center", width=bin_width, alpha=0.25)
