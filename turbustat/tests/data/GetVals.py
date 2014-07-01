117af1cf67c125ac2497180d8df203b539c88f7a
e-koch <koch.eric.w@gmail.com>
2014-07-01 12:12:43 -0700

    Finished testing suite data sets.

---
+++ turbustat/tests/data/GetVals.py
@@ -0,0 +1,169 @@
+
+'''
+Save the key results using the testing datasets.
+'''
+
+import numpy as np
+from turbustat.io import fromfits
+import sys
+
+keywords = ["centroid", "centroid_error", "integrated_intensity",
+            "integrated_intensity_error", "linewidth", "linewidth_error",
+            "moment0", "moment0_error", "cube"]
+
+
+from turbustat.tests._testing_data import dataset1, dataset2
+
+## Wavelet Transform
+
+from turbustat.statistics import Wavelet_Distance
+
+wavelet_distance = Wavelet_Distance(dataset1["integrated_intensity"],
+                                    dataset2["integrated_intensity"]).distance_metric(verbose=True)
+
+wavelet_val = wavelet_distance.wt1.curve
+
+## MVC#
+
+from turbustat.statistics import MVC_distance
+
+mvc_distance = MVC_distance(dataset1, dataset2).distance_metric(verbose=True)
+
+mvc_val = mvc_distance.mvc1.ps1D
+
+## Spatial Power Spectrum/ Bispectrum#
+
+from turbustat.statistics import PSpec_Distance, BiSpectrum_Distance
+
+pspec_distance = PSpec_Distance(dataset1, dataset2).distance_metric(verbose=True)
+
+pspec_val = pspec_distance.pspec1.ps1D
+
+bispec_distance = BiSpectrum_Distance(dataset1["integrated_intensity"],
+                                      dataset2["integrated_intensity"]).distance_metric(verbose=True)
+
+bispec_val = bispec_distance.bispec1.bicoherence
+
+## Genus
+
+from turbustat.statistics import GenusDistance
+
+genus_distance = GenusDistance(dataset1["integrated_intensity"][0],
+                               dataset2["integrated_intensity"][0]).distance_metric(verbose=True)
+
+genus_val = genus_distance.genus1.genus_stats
+
+## Delta-Variance
+
+from turbustat.statistics import DeltaVariance_Distance
+
+delvar_distance = \
+    DeltaVariance_Distance(dataset1["integrated_intensity"],
+                           dataset1["integrated_intensity_error"][0],
+                           dataset2["integrated_intensity"],
+                           dataset2["integrated_intensity_error"][0])
+
+delvar_val = delvar_distance.delvar1.delta_var
+
+## VCA/VCS
+
+from turbustat.statistics import VCA_Distance, VCS_Distance
+
+vcs_distance = VCS_Distance(dataset1["cube"],
+                            dataset2["cube"]).distance_metric(verbose=True)
+
+vcs_val = vcs_distance.vcs1.ps1D
+
+vca_distance = VCA_Distance(dataset1["cube"],
+                            dataset2["cube"]).distance_metric(verbose=True)
+
+vca_val = vca_distance.vca1.ps1D
+
+## Tsallis#
+
+from turbustat.statistics import Tsallis_Distance
+
+tsallis_distance = \
+    Tsallis_Distance(dataset1["integrated_intensity"][0],
+                     dataset2["integrated_intensity"][0],
+                     lags=[1, 2, 4, 8, 16],
+                     num_bins=100).distance_metric(verbose=True)
+
+tsallis_val = tsallis_distance.tsallis1.tsallis_fits
+
+# High-order stats
+
+from turbustat.statistics import StatMomentsDistance
+
+moment_distance = \
+    StatMomentsDistance(dataset1["integrated_intensity"][0],
+                        dataset2["integrated_intensity"][0], 5).distance_metric(verbose=True)
+
+kurtosis_val = moment_distance.moments1.kurtosis_hist[1]
+skewness_val = moment_distance.moments1.skewness_hist[1]
+
+## PCA
+
+from turbustat.statistics import PCA_Distance
+
+pca_distance = PCA_Distance(dataset1["cube"][0],
+                            dataset2["cube"][0]).distance_metric(verbose=True)
+pca_val = pca_distance.pca1.eigvals
+
+## SCF
+
+from turbustat.statistics import SCF_Distance
+
+scf_distance = SCF_Distance(dataset1["cube"][0],
+                            dataset2["cube"][0]).distance_metric(verbose=True)
+scf_val = scf_distance.scf1.scf_surface
+
+## Cramer Statistic
+
+from turbustat.statistics import Cramer_Distance
+
+cramer_distance = Cramer_Distance(dataset1["cube"][0],
+                                  dataset2["cube"][0]).distance_metric()
+
+cramer_val = cramer_distance.data_matrix1
+
+## Dendrograms
+
+from turbustat.statistics import DendroDistance
+
+dendro_distance = DendroDistance(dataset1["cube"][0],
+                                 dataset2["cube"][0]).distance_metric(verbose=True)
+
+dendrogram_val = dendro_distance.dendro1.numfeatures
+
+
+np.savez_compressed('checkVals', wavelet_val=wavelet_val,
+                    mvc_val=mvc_val,
+                    pspec_val=pspec_val,
+                    bispec_val=bispec_val,
+                    genus_val=genus_val,
+                    delvar_val=delvar_val,
+                    vcs_val=vcs_val,
+                    vca_val=vca_val,
+                    tsallis_val=tsallis_val,
+                    kurtosis_val=kurtosis_val,
+                    skewness_val=skewness_val,
+                    pca_val=pca_val,
+                    scf_val=scf_val,
+                    cramer_val=cramer_val,
+                    dendrogram_val=dendrogram_val)
+
+np.savez_compressed('computed_distances', mvc_distance=mvc_distance.distance,
+                    pca_distance=pca_distance.distance,
+                    vca_distance=vca_distance.distance,
+                    pspec_distance=pspec_distance.distance,
+                    scf_distance=scf_distance.distance,
+                    wavelet_distance=wavelet_distance.distance,
+                    delvar_distance=delvar_distance.distance,
+                    tsallis_distance=tsallis_distance.distance,
+                    kurtosis_distance=moment_distance.kurtosis_distance,
+                    skewness_distance=moment_distance.skewness_distance,
+                    cramer_distance=cramer_distance.distance,
+                    genus_distance=genus_distance.distance,
+                    vcs_distance=vcs_distance.distance,
+                    bispec_distance=bispec_distance.distance)
