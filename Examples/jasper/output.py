9b9bbf71e67560a11ed74867df9e75619d01f247
e-koch <koch.eric.w@gmail.com>
2014-07-16 17:18:01 -0700

    Try running without dendrogram methods.

--- Examples/jasper/output.py
+++ Examples/jasper/output.py
@@ -291,7 +291,7 @@ if __name__ == "__main__":
     statistics = ["Wavelet", "MVC", "PSpec", "Bispectrum", "DeltaVariance",
                   "Genus", "VCS", "VCA", "Tsallis", "PCA", "SCF", "Cramer",
                   "Skewness", "Kurtosis", "VCS_Density", "VCS_Velocity",
-                  "Dendrogram_Hist", "Dendrogram_Num", "PDF"]
+                  "PDF"]  # "Dendrogram_Hist", "Dendrogram_Num"]
 
     print "Statistics to run: %s" % (statistics)
     num_statistics = len(statistics)
