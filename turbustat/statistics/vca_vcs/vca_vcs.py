a02648fe009d0a2c3922af08a690394f7d46ee2a
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2014-10-01 09:50:04 +0200

    if the first channel is all zeros (e.g., padded to allow for different # of slice sizes in VCA), need a different way to define good "channels" (is the definition of channels right anyway?  seems like this is good spatial pixels...)

--- turbustat/statistics/vca_vcs/vca_vcs.py
+++ turbustat/statistics/vca_vcs/vca_vcs.py
@@ -175,7 +175,7 @@ class VCS(object):
         if np.isnan(self.cube).any():
             self.cube[np.isnan(self.cube)] = 0
             # Feel like this should be more specific
-            self.good_channel_count = np.sum(self.cube[0, :, :] != 0)
+            self.good_channel_count = np.sum(self.cube.max(axis=0) != 0)
         else:
             self.good_channel_count = float(
                 self.cube.shape[1] * self.cube.shape[2])
