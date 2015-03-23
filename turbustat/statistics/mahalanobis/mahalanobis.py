f296b21791bd6503ead8f218bef65e825a65aa7b
e-koch <koch.eric.w@gmail.com>
2015-03-23 16:44:56 -0600

    Mahalanobis distance based on Mantel test.

--- turbustat/statistics/mahalanobis/mahalanobis.py
+++ turbustat/statistics/mahalanobis/mahalanobis.py
@@ -3,6 +3,7 @@ import numpy as np
 from scipy.spatial.distance import mahalanobis
 
 from ..threeD_to_twoD import _format_data
+from ..mantel import mantel_test
 
 
 class Mahalanobis(object):
@@ -22,7 +23,7 @@ class Mahalanobis(object):
 
         return self
 
-    def compute_distmat(self, n_jobs=1):
+    def compute_distmat(self):
         '''
         Compute the Mahalanobis distance matrix.
         '''
@@ -41,12 +42,12 @@ class Mahalanobis(object):
 
         return self
 
-    def run(self, verbose=False, n_jobs=1, *args):
+    def run(self, verbose=False, *args):
         '''
         Run all computations.
         '''
         self.format_data(*args)
-        self.compute_distmat(n_jobs=n_jobs)
+        self.compute_distmat()
 
         if verbose:
             import matplotlib.pyplot as p
@@ -62,101 +63,24 @@ class Mahalanobis_Distance(object):
 
     """docstring for Mahalanobis_Distance"""
 
-    def __init__(self, cube1, cube2, noise_value1=0.1,
-                 noise_value2=0.1, data_format="intensity"):
+    def __init__(self, cube1, cube2):
         super(Mahalanobis_Distance, self).__init__()
-        self.cube1 = cube1
-        self.cube2 = cube2
-        self.data_format = data_format
 
-        self.noise_value1 = noise_value1
-        self.noise_value2 = noise_value2
+        self.mahala1 = Mahalanobis(cube1)
+        self.mahala2 = Mahalanobis(cube2)
 
-        self.data_matrix1 = None
-        self.data_matrix2 = None
-        self.distance = None
-
-    def format_data(self, data_format=None, num_spec=1000):
+    def compute_distmats(self, data_format='spectra', *args):
         '''
-        Rearrange data into a 2D object using the given format.
+        Create a 2D representation of the data. args are passed to
+        _format_data.
         '''
 
-        if data_format is not None:
-            self.data_format = data_format
-
-        if self.data_format is "spectra":
-            if num_spec is None:
-                raise ValueError('Must specify num_spec for data format',
-                                 'spectra.')
-
-            # Find the brightest spectra in the cube
-            mom0 = np.nansum(self.cube1)
-
-            bright_spectra = \
-                np.argpartition(mom0.ravel(), -num_spec)[-num_spec:]
-
-            x = np.empty((num_spec,))
-            y = np.empty((num_spec,))
-
-            for i in range(num_spec):
-                x[i] = bright_spectra[i] / self.cube1.shape[1]
-                y[i] = bright_spectra[i] % self.cube1.shape[2]
-
-            self.data_matrix1 = self.cube1[:, x, y]
-
-        elif self.data_format is "intensity":
-            self.data_matrix1 = intensity_data(self.cube1,
-                                               noise_lim=self.noise_value1)
-            self.data_matrix2 = intensity_data(self.cube2,
-                                               noise_lim=self.noise_value2)
-
-        else:
-            raise NameError(
-                "data_format must be either 'spectra' or 'intensity'.")
+        self.mahala1.format_data(data_format=data_format, *args)
+        self.mahala2.format_data(data_format=data_format, *args)
 
         return self
 
-    def mahalanobis_statistic(self, n_jobs=1):
-        '''
-
-        '''
-        # Adjust what we call n,m based on the larger dimension.
-        # Then the looping below is valid.
-        if self.data_matrix1.shape[1] >= self.data_matrix2.shape[1]:
-            m = self.data_matrix1.shape[1]
-            n = self.data_matrix2.shape[1]
-        else:
-            n = self.data_matrix1.shape[1]
-            m = self.data_matrix2.shape[1]
-
-        term1 = 0.0
-        term2 = 0.0
-        term3 = 0.0
-
-        for i in range(m):
-            for j in range(n):
-                term1 += mahala_fcn(self.data_matrix1[:, i],
-                                    self.data_matrix2[:, j])
-            for ii in range(m):
-                term2 += mahala_fcn(self.data_matrix1[:, i],
-                                    self.data_matrix1[:, ii])
-
-            if i <= n:
-                for jj in range(n):
-                    term3 += mahala_fcn(
-                        self.data_matrix2[:, i], self.data_matrix2[:, jj])
-
-        m, n = float(m), float(n)
-
-        term1 *= (1 / (m * n))
-        term2 *= (1 / (2 * m**2.))
-        term3 *= (1 / (2 * n**2.))
-
-        self.distance = (m * n / (m + n)) * (term1 - term2 - term3)
-
-        return self
-
-    def distance_metric(self, n_jobs=1):
+    def distance_metric(self, correlation='pearson', verbose=False):
         '''
 
         This serves as a simple wrapper in order to remain with the coding
@@ -164,8 +88,21 @@ class Mahalanobis_Distance(object):
 
         '''
 
-        self.format_data()
-        self.mahalanobis_statistic(n_jobs=n_jobs)
+        self.distance, self.pval = \
+            mantel_test(self.mahala1.distance_matrix,
+                        self.mahala2.distance_matrix,
+                        corr_func=correlation)
+
+        if verbose:
+            import matplotlib.pyplot as p
+
+            p.subplot(121)
+            p.title('Results: Distance - '+str(self.distance))
+            p.imshow(self.mahala1.distance_matrix, interpolation='nearest')
+            p.subplot(122)
+            p.title('Results: P-value - '+str(self.pval))
+            p.imshow(self.mahala2.distance_matrix, interpolation='nearest')
+            p.show()
 
         return self
 
