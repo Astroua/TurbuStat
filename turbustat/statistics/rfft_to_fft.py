fcc54b433b2525101c7302d882c8976f50e72263
e-koch <koch.eric.w@gmail.com>
2015-04-02 13:44:03 -0600

    Fixed issues with rfft to fft conversion

--- turbustat/statistics/rfft_to_fft.py
+++ turbustat/statistics/rfft_to_fft.py
@@ -20,29 +20,23 @@ def rfft_to_fft(image):
 
     fft_abs = np.abs(np.fft.rfftn(image))
 
-
     if ndim == 2:
         if last_dim % 2 == 0:
-            fftstar_abs = fft_abs[:, -2:0:-1]
+            fftstar_abs = fft_abs.copy()[:, -2:0:-1]
         else:
-            fftstar_abs = fft_abs[:, -1:0:-1]
+            fftstar_abs = fft_abs.copy()[:, -1:0:-1]
 
         fftstar_abs[1::, :] = fftstar_abs[:0:-1, :]
-
-        fft_abs = np.roll(fft_abs[::-1, :], 1, axis=0)
 
         return np.concatenate((fft_abs, fftstar_abs), axis=1)
 
     elif ndim == 3:
         if last_dim % 2 == 0:
-            fftstar_abs = fft_abs[:, :, -2:0:-1]
+            fftstar_abs = fft_abs.copy()[:, :, -2:0:-1]
         else:
-            fftstar_abs = fft_abs[:, :, -1:0:-1]
+            fftstar_abs = fft_abs.copy()[:, :, -1:0:-1]
 
         fftstar_abs[1::, :, :] = fftstar_abs[:0:-1, :, :]
         fftstar_abs[:, 1::, :] = fftstar_abs[:, :0:-1, :]
-
-        fft_abs = np.roll(fft_abs[::-1, :, :], 1, axis=0)
-        fft_abs = np.roll(fft_abs[:, ::-1, :], 1, axis=1)
 
         return np.concatenate((fft_abs, fftstar_abs), axis=2)
