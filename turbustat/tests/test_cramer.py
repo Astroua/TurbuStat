4d3a2052929f64a4c99c3725d2e0898012b02e5a
e-koch <koch.eric.w@gmail.com>
2015-05-12 14:00:22 +0200

    Run distance metric

--- turbustat/tests/test_cramer.py
+++ turbustat/tests/test_cramer.py
@@ -34,7 +34,9 @@ class testCramer(TestCase):
         small_data = dataset1["cube"][0][:, :26, :26]
 
         self.tester2 = Cramer_Distance(small_data, dataset2["cube"][0])
+        self.tester2.distance_metric()
         self.tester3 = Cramer_Distance(dataset2["cube"][0], small_data)
+        self.tester3.distance_metric()
 
         npt.assert_almost_equal(self.tester2.distance, self.tester3.distance)
 
