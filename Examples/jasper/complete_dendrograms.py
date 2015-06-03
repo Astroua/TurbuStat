12da0bf7eb139d4879a20ff2f2c956001000371d
e-koch <koch.eric.w@gmail.com>
2015-06-03 10:05:34 -0600

    It helps to open the files...

--- Examples/jasper/complete_dendrograms.py
+++ Examples/jasper/complete_dendrograms.py
@@ -19,7 +19,8 @@ min_deltas = np.logspace(-0.5, 0.5, 20)
 
 print("Running NGC1333.")
 
-ngc1333 = Dendrogram_Stats("/home/ekoch/sims/complete/ngc1333.13co.fits",
+ngc1333_data = fits.getdata("/home/ekoch/sims/complete/ngc1333.13co.fits")
+ngc1333 = Dendrogram_Stats(ngc1333_data,
                            min_deltas=min_deltas,
                            dendro_params={"min_npix": 100,
                                           "min_value": noise_ngc1333})
@@ -29,7 +30,8 @@ ngc1333.run(save_results=True, output_name="ngc1333.13co_dendrostat.pkl",
 
 print("Running OphA.")
 
-ophA = Dendrogram_Stats("/home/ekoch/sims/complete/ophA.13co.fits",
+ophA_data = fits.getdata("/home/ekoch/sims/complete/ophA.13co.fits")
+ophA = Dendrogram_Stats(ophA_data,
                         min_deltas=min_deltas,
                         dendro_params={"min_npix": 100,
                                        "min_value": noise_ophA})
@@ -39,7 +41,8 @@ ophA.run(save_results=True, output_name="ophA.13co_dendrostat.pkl",
 
 print("Running IC 348.")
 
-ic348 = Dendrogram_Stats("/home/ekoch/sims/complete/ic348.13co.fits",
+ic348_data = fits.getdata("/home/ekoch/sims/complete/ic348.13co.fits")
+ic348 = Dendrogram_Stats(ic348_data,
                          min_deltas=min_deltas,
                          dendro_params={"min_npix": 100,
                                         "min_value": noise_ic348})
