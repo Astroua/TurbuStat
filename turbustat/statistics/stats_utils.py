21712c1a790ca5abc753c07e105c64be2e4938a7
e-koch <koch.eric.w@gmail.com>
2015-09-17 18:23:01 -0600

    Common histogram bins function

--- turbustat/statistics/stats_utils.py
+++ turbustat/statistics/stats_utils.py
@@ -47,3 +47,27 @@ def kl_divergence(P, Q):
     P = P[np.isfinite(P)]
     Q = Q[np.isfinite(Q)]
     return np.nansum(np.where(Q != 0, P * np.log(P / Q), 0))
+
+
+def common_histogram_bins(dataset1, dataset2, nbins=None, logscale=False):
+    '''
+    Returns bins appropriate for both given datasets. If nbins is not
+    specified, the number is set by the square root of the average
+    number of elements in the datasets. This assumes that there are many
+    (~>100) elements in each dataset.
+    '''
+
+    if dataset1.ndim > 1 or dataset2.ndim > 1:
+        raise ValueError("dataset1 and dataset2 should be 1D arrays.")
+
+    global_min = min(np.nanmin(dataset1), np.nanmin(dataset2))
+    global_max = max(np.nanmax(dataset1), np.nanmax(dataset2))
+
+    if nbins is None:
+        nbins = int(np.sqrt((dataset1.size + dataset2.size)/2.))
+
+    if logscale:
+        return np.logspace(np.log10(global_min),
+                           np.log10(global_max), num=nbins)
+
+    return np.linspace(global_min, global_max, num=nbins)
