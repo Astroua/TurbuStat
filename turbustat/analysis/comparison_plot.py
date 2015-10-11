586416ff09686174c1e4307eeace6db37612c928
e-koch <koch.eric.w@gmail.com>
2015-10-11 16:36:48 -0600

    Change order again

--- turbustat/analysis/comparison_plot.py
+++ turbustat/analysis/comparison_plot.py
@@ -339,7 +339,7 @@ def _horiz_obs_plot(ax, data, num_fids, shading=False, legend=False):
                    "ic348.13co.fits": "IC 348"}
 
     # Also needs to be generalized
-    colors = ["b", "g", "r"]
+    colors = ["g", "r", "b"]
 
     x_vals = ax.axis()[:2]
 
