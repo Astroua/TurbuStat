1a788a3dbf3b08d459fde43629ecc947b3e52947
e-koch <koch.eric.w@gmail.com>
2016-02-29 15:51:50 -0700

    Return distances instead of frequencies

--- turbustat/statistics/psds.py
+++ turbustat/statistics/psds.py
@@ -4,7 +4,7 @@ from scipy.stats import binned_statistic
 
 
 def pspec(psd2, nbins=None, return_stddev=False, binsize=1.0,
-          logspacing=True, max_bin=None, min_bin=None):
+          logspacing=True, max_bin=None, min_bin=None, return_freqs=True):
     '''
     Calculate the radial profile using scipy.stats.binned_statistic.
 
@@ -26,6 +26,8 @@ def pspec(psd2, nbins=None, return_stddev=False, binsize=1.0,
         Give the maximum value to bin to.
     min_bin : float, optional
         Give the minimum value to bin to.
+    return_freqs : bool, optional
+        Return spatial frequencies.
 
     Returns
     -------
@@ -38,16 +40,6 @@ def pspec(psd2, nbins=None, return_stddev=False, binsize=1.0,
         within each of the bins.
     '''
 
-    yfreqs = np.fft.fftshift(np.abs(np.fft.fftfreq(psd2.shape[0])))
-    xfreqs = np.fft.fftshift(np.abs(np.fft.fftfreq(psd2.shape[1])))
-
-    yy_freq, xx_freq = np.meshgrid(yfreqs, xfreqs)
-
-    freqs_dist = np.sqrt(yy_freq**2 + xx_freq**2)
-
-    zero_freq_val = freqs_dist[np.nonzero(freqs_dist)].min() / 2.
-    freqs_dist[freqs_dist == 0] = zero_freq_val
-
     y = np.arange(psd2.shape[0] - np.floor(psd2.shape[0]/2.).astype(int))
     x = np.arange(psd2.shape[1] - np.floor(psd2.shape[1]/2.).astype(int))
 
@@ -58,18 +50,40 @@ def pspec(psd2, nbins=None, return_stddev=False, binsize=1.0,
     if nbins is None:
         nbins = int(np.round(dists.max() / binsize)+1)
 
+    if return_freqs:
+        yfreqs = np.fft.fftshift(np.abs(np.fft.fftfreq(psd2.shape[0])))
+        xfreqs = np.fft.fftshift(np.abs(np.fft.fftfreq(psd2.shape[1])))
+
+        yy_freq, xx_freq = np.meshgrid(yfreqs, xfreqs)
+
+        freqs_dist = np.sqrt(yy_freq**2 + xx_freq**2)
+
+        zero_freq_val = freqs_dist[np.nonzero(freqs_dist)].min() / 2.
+        freqs_dist[freqs_dist == 0] = zero_freq_val
+
     if max_bin is None:
-        max_bin = freqs_dist.flatten()[np.argmax(dists)]
+        if return_freqs:
+            max_bin = freqs_dist.flatten()[np.argmax(dists)]
+        else:
+            max_bin = dists.max()
 
     if min_bin is None:
-        min_bin = zero_freq_val
+        if return_freqs:
+            min_bin = zero_freq_val
+        else:
+            min_bin = 0.5
 
     if logspacing:
         bins = np.logspace(np.log10(min_bin), np.log10(max_bin), nbins+1)
     else:
         bins = np.linspace(min_bin, max_bin, nbins+1)
 
-    ps1D, bin_edge, cts = binned_statistic(freqs_dist.ravel(),
+    if return_freqs:
+        dist_arr = freqs_dist
+    else:
+        dist_arr = dists
+
+    ps1D, bin_edge, cts = binned_statistic(dist_arr.ravel(),
                                            psd2.ravel(),
                                            bins=bins,
                                            statistic=np.nanmean)
@@ -79,7 +93,7 @@ def pspec(psd2, nbins=None, return_stddev=False, binsize=1.0,
     if not return_stddev:
         return bin_cents, ps1D
     else:
-        ps1D_stddev = binned_statistic(freqs_dist.ravel(),
+        ps1D_stddev = binned_statistic(dist_arr.ravel(),
                                        psd2.ravel(),
                                        bins=bins,
                                        statistic=np.nanstd)[0]
