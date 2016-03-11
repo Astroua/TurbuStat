86e9eec7bfd5ec1db066962fb5de5d471e3f33a0
e-koch <koch.eric.w@gmail.com>
2016-03-11 09:26:08 -0700

    More name changes

--- turbustat/statistics/statistics_list.py
+++ turbustat/statistics/statistics_list.py
@@ -5,13 +5,13 @@ Returns a list of all available distance metrics
 '''
 
 statistics_list = ["Wavelet", "MVC", "PSpec", "Bispectrum", "DeltaVariance",
-                  "Genus", "VCS", "VCA", "Tsallis", "PCA", "SCF", "Cramer",
-                  "Skewness", "Kurtosis", "VCS_Density", "VCS_Velocity",
-                  "PDF_Hellinger", "PDF_KS", # "PDF_AD",
-                  "Dendrogram_Hist", "Dendrogram_Num"]
+                   "Genus", "VCS", "VCA", "Tsallis", "PCA", "SCF", "Cramer",
+                   "Skewness", "Kurtosis", "VCS_Small_Scale",
+                   "VCS_Large_Scale", "PDF_Hellinger", "PDF_KS",  # "PDF_AD",
+                   "Dendrogram_Hist", "Dendrogram_Num"]
 
 twoD_statistics_list = \
     ["Wavelet", "MVC", "PSpec", "Bispectrum", "DeltaVariance",
      "Genus", "Tsallis", "Skewness", "Kurtosis",
-     "PDF_Hellinger", "PDF_KS", # "PDF_AD",
+     "PDF_Hellinger", "PDF_KS",  # "PDF_AD",
      "Dendrogram_Hist", "Dendrogram_Num"]
