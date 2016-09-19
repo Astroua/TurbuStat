cb160171abcb9ee664b50bb49d874d0a20ccafcd
e-koch <koch.eric.w@gmail.com>
2016-09-19 15:38:12 -0600

    Test if between values

--- turbustat/tests/_testing_data.py
+++ turbustat/tests/_testing_data.py
@@ -160,3 +160,18 @@ def generate_1D_array(shape=200, curve_type='gaussian', **kwargs):
     xgrid = np.arange(shape)
 
     return mod(xgrid)
+
+
+def assert_between(value, lower, upper):
+    '''
+    Check if a value is between two values.
+    '''
+
+    within_lower = value >= lower
+    within_upper = value <= upper
+
+    if within_lower and within_upper:
+        return
+    else:
+        raise AssertionError("{0} not within {1} and {2}".format(value, lower,
+                                                                 upper))
