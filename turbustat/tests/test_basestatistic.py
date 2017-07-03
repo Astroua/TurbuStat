3a81819656d46c3ab4522da02cf208050f6e0602
e-koch <koch.eric.w@gmail.com>
2017-07-03 17:14:56 -0600

    Add tests for all unit conversions

--- turbustat/tests/test_basestatistic.py
+++ turbustat/tests/test_basestatistic.py
@@ -1,10 +1,13 @@
 
 import pytest
 import astropy.units as u
+import numpy.testing as npt
+import numpy as np
 
 from turbustat.statistics.base_statistic import BaseStatisticMixIn
 
 from ._testing_data import header
+from .helpers import assert_allclose
 
 
 @pytest.mark.xfail(raises=AttributeError)
@@ -30,9 +33,9 @@ def test_angles():
 
     obj.header = header
 
-    obj.ang_size
+    obj._ang_size
 
-    obj.angular_equiv
+    obj._angular_equiv
 
 
 def test_distance():
@@ -43,8 +46,8 @@ def test_distance():
     obj.distance = 1 * u.cm
 
     obj.distance
-    obj.distance_equiv
-    obj.distance_size
+    obj._physical_equiv
+    obj._physical_size
 
 
 @pytest.mark.xfail(raises=AttributeError)
@@ -79,8 +82,8 @@ def test_spectral():
 
     obj.header = header
 
-    obj.spectral_size
-    obj.spectral_equiv
+    obj._spectral_size
+    obj._spectral_equiv
 
 
 @pytest.mark.xfail(raises=ValueError)
@@ -94,3 +97,75 @@ def test_no_spectral():
     obj.header = celestial_header
 
     obj.spectral_size
+
+
+def test_pixel_to_spatial_conversions():
+
+    obj = BaseStatisticMixIn()
+
+    obj.header = header
+    obj.distance = 1 * u.pc
+
+    assert_allclose(obj._to_angular(1 * u.pix, u.deg),
+                    header["CDELT2"] * u.deg)
+
+    assert_allclose(obj._to_physical(1 * u.pix, u.pc),
+                    (header["CDELT2"] * np.pi / 180.) * u.pc)
+
+    assert_allclose(obj._spatial_unit_conversion(1 * u.pix, u.deg),
+                    header["CDELT2"] * u.deg)
+
+    assert_allclose(obj._spatial_unit_conversion(1 * u.pix, u.pc),
+                    (header["CDELT2"] * np.pi / 180.) * u.pc)
+
+    assert_allclose(obj._to_pixel(header["CDELT2"] * u.deg),
+                    1.0 * u.pix)
+
+    assert_allclose(obj._to_pixel((header["CDELT2"] * np.pi / 180.) * u.pc),
+                    1.0 * u.pix)
+
+
+def test_pixel_to_spatial_freq_conversions():
+
+    obj = BaseStatisticMixIn()
+
+    obj.header = header
+    obj.distance = 1 * u.pc
+
+    assert_allclose(obj._spatial_freq_unit_conversion(1 / u.pix, 1 / u.deg),
+                    1 / (header["CDELT2"] * u.deg))
+
+    assert_allclose(obj._spatial_freq_unit_conversion(1 / u.pix, 1 / u.pc),
+                    1 / ((header["CDELT2"] * np.pi / 180.) * u.pc))
+
+    assert_allclose(obj._to_pixel_freq(1 / (header["CDELT2"] * u.deg)),
+                    1 / u.pix)
+
+    assert_allclose(obj._to_pixel_freq(1 / ((header["CDELT2"] * np.pi / 180.) * u.pc)),
+                    1 / u.pix)
+
+
+def test_pixel_to_spectral_conversions():
+
+    obj = BaseStatisticMixIn()
+
+    obj.header = header
+
+    assert_allclose(obj._to_spectral(1 * u.pix, u.m / u.s),
+                    np.abs(header["CDELT3"]) * u.m / u.s)
+
+    assert_allclose(obj._to_spectral(np.abs(header["CDELT3"]) * u.m / u.s, u.pix),
+                    1 * u.pix)
+
+
+def test_pixel_to_spectral_freq_conversions():
+
+    obj = BaseStatisticMixIn()
+
+    obj.header = header
+
+    assert_allclose(1 / obj._spectral_freq_unit_conversion(1 / u.pix, u.s / u.m),
+                    np.abs(header["CDELT3"]) * u.m / u.s)
+
+    assert_allclose(obj._spectral_freq_unit_conversion( 1/ (np.abs(header["CDELT3"]) * u.m / u.s), 1 / u.pix),
+                    1 / u.pix)
