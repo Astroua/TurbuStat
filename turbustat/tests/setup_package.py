8cab1d360218f6d8075bad08fd38ef90c75e5549
e-koch <koch.eric.w@gmail.com>
2017-08-19 08:19:28 -0700

    Add coveragerc to package data

--- turbustat/tests/setup_package.py
+++ turbustat/tests/setup_package.py
@@ -1,5 +1,6 @@
 
 def get_package_data():
     return {
-        _ASTROPY_PACKAGE_NAME_ + '.tests': ['data/*.fits', 'data/*.npz']
+        _ASTROPY_PACKAGE_NAME_ + '.tests': ['data/*.fits', 'data/*.npz',
+                                            'coveragerc']
     }
