adb600f6745b4f7341d0a2e9f00336d335c430e2
e-koch <koch.eric.w@gmail.com>
2017-09-10 15:49:47 -0600

    Generalize the azimuthal test for other parameters

--- turbustat/tests/test_pspec.py
+++ turbustat/tests/test_pspec.py
@@ -1,14 +1,14 @@
 # Licensed under an MIT open source license - see LICENSE
 from __future__ import print_function, absolute_import, division
 
-import numpy as np
+import pytest
 import numpy.testing as npt
 import astropy.units as u
 from astropy.io import fits
 
 from ..statistics import PowerSpectrum, PSpec_Distance
-from ._testing_data import \
-    dataset1, dataset2, computed_data, computed_distances, make_extended
+from ._testing_data import (dataset1, dataset2, computed_data,
+                            computed_distances, make_extended, assert_between)
 
 
 def test_PSpec_method():
@@ -68,33 +68,33 @@ def test_pspec_nonequal_shape():
     npt.assert_almost_equal(test.slope2D, test_T.slope2D, decimal=3)
 
 
-def test_pspec_azimlimits():
+@pytest.mark.parametrize(('plaw', 'ellip'),
+                         [(plaw, ellip) for plaw in [3, 4]
+                          for ellip in [0.2, 0.5, 0.75, 0.9, 1.0]])
+def test_pspec_azimlimits(plaw, ellip):
 
     imsize = 256
-    plaw = 3.0
+    theta = 0
 
     # Generate a red noise model
-    img = make_extended(imsize, powerlaw=plaw, ellip=0.5, theta=0.,
+    img = make_extended(imsize, powerlaw=plaw, ellip=ellip, theta=theta,
                         return_psd=False)
 
     test = PowerSpectrum(fits.PrimaryHDU(img))
     test.run(radial_pspec_kwargs={"theta_0": 0 * u.deg,
-                                  "delta_theta": 10 * u.deg},
+                                  "delta_theta": 30 * u.deg},
              fit_2D=False, weighted_fit=True)
 
     test2 = PowerSpectrum(fits.PrimaryHDU(img))
     test2.run(radial_pspec_kwargs={"theta_0": 90 * u.deg,
-                                   "delta_theta": 10 * u.deg},
+                                   "delta_theta": 30 * u.deg},
               fit_2D=False, weighted_fit=True)
 
     test3 = PowerSpectrum(fits.PrimaryHDU(img))
     test3.run(radial_pspec_kwargs={},
               fit_2D=False, weighted_fit=True)
 
-    # Ensure slopes are consistent to within 2 sigma
-    npt.assert_allclose(-plaw, test3.slope, rtol=.05)
-    npt.assert_allclose(-plaw, test2.slope, rtol=.05)
-
-    # The aligned slope need not be consistent with the given plaw
-    # It should be flatter.
-    assert test.slope > test2.slope
+    # Ensure slopes are consistent to within 5%
+    assert_between(test3.slope, - 1.05 * plaw, - 0.95 * plaw)
+    assert_between(test2.slope, - 1.05 * plaw, - 0.95 * plaw)
+    assert_between(test.slope, - 1.05 * plaw, - 0.95 * plaw)
