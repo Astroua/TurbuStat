Corrected weighting in SCF distance_metric.
Commit addresses the error described here: https://github.com/Astroua/TurbuStat/issues/184

turbustat/statistics/scf/scf.py
@@ -829,10 +829,9 @@ class SCF_Distance(object):
 
         a, b = np.meshgrid(dx, dy)
         if self.weighted:
-            # Centre pixel set to 1
-            a[np.where(a == 0)] = 1.
-            b[np.where(b == 0)] = 1.
             dist_weight = 1 / np.sqrt(a ** 2 + b ** 2)
+            # Centre pixel set to 1
+            dist_weight[np.where((a == 0) & (b == 0))] = 1.
         else:
             dist_weight = np.ones((self.size, self.size))
 