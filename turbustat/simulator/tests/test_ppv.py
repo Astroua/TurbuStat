c5cec60bc6b4c3e785bd616bcb5d5cff5770e408
e-koch <koch.eric.w@gmail.com>
2018-08-13 16:25:21 -0600

    Add PPV tests along all LOS; correct lwidth comparisons

--- turbustat/simulator/tests/test_ppv.py
+++ turbustat/simulator/tests/test_ppv.py
@@ -6,6 +6,7 @@ import pytest
 import numpy as np
 import numpy.testing as npt
 import astropy.units as u
+import astropy.constants as c
 
 try:
     from spectral_cube import SpectralCube
@@ -15,18 +16,29 @@ except ImportError:
 
 
 @pytest.mark.skipif("not SPECCUBE_INSTALL")
-def test_ppv():
+@pytest.mark.parametrize('axis', [0, 1, 2])
+# @pytest.mark.parametrize('axis', [0])
+def test_ppv(axis):
     '''
     Ensure the column density matches the expected value in the output cube
     '''
+
+    # Number of samples to take along each non-projected dimension
+    size = 2
+
+    twod_slice = [slice(0, size), slice(0, size)]
+    threed_slice = [slice(0, size), slice(0, size)]
+    threed_slice.insert(axis, slice(None))
 
     # Need a large enough field to have good statistics
     velocity = make_3dfield(128, powerlaw=3.5, amp=5.e3) * u.m / u.s
 
     density = np.ones_like(velocity.value) * u.cm**-3
 
-    cube_hdu = make_ppv(velocity[:, :2, :2], density[:, :2, :2],
-                        vel_disp=np.std(velocity), T=100 * u.K,
+    cube_hdu = make_ppv(velocity[threed_slice], density[threed_slice],
+                        los_axis=axis,
+                        vel_disp=np.std(velocity, axis=axis)[twod_slice].max(),
+                        T=100 * u.K,
                         return_hdu=True,
                         chan_width=500 * u.m / u.s)
 
@@ -42,19 +54,23 @@ def test_ppv():
 
     assert NHI_exp.unit == NHI_cube.unit
 
+    # Expected is 3.0854e18. Check if it is within 1e16
     npt.assert_allclose(NHI_exp.value, NHI_cube.value, rtol=1e-4)
 
     # Rough comparison of line width to the velocity field std.
     # Very few samples, so this is only a rough check
-    lwidth = cube.linewidth_sigma().to(u.km / u.s)
-    vel_std = np.std(velocity).to(u.km / u.s)
+    v_therm = np.sqrt(c.k_B * 100 * u.K / (1.4 * c.m_p)).to(u.km / u.s)
 
-    npt.assert_allclose(vel_std.value, lwidth.value, rtol=0.2)
+    # Correct the measured line widths for thermal broadening
+    lwidth = np.sqrt(cube.linewidth_sigma().to(u.km / u.s)**2 - v_therm**2)
+    vel_std = np.std(velocity, axis=0)[twod_slice].to(u.km / u.s)
+
+    npt.assert_allclose(vel_std.value, lwidth.value, atol=v_therm.value)
 
     # Compare centroids
 
-    raw_centroid = ((velocity[:, :2, :2] * density[:, :2, :2]).sum(0) /
-                    (density[:, :2, :2]).sum(0)).to(u.km / u.s)
+    raw_centroid = ((velocity[threed_slice] * density[threed_slice]).sum(axis) /
+                    (density[threed_slice]).sum(axis)).to(u.km / u.s)
     mom1 = cube.moment1().to(u.km / u.s)
 
-    npt.assert_allclose(raw_centroid.value, mom1.value, rtol=1e-3)
+    npt.assert_allclose(raw_centroid.value, mom1.value, atol=v_therm.value)
