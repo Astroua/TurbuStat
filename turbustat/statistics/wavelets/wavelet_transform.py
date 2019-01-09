aa1a18d39eaefdbc7b9afe10b015b753cdf7b38a
e-koch <koch.eric.w@gmail.com>
2019-01-09 13:31:10 -0700

    Catch floating point rounding issues in Wavelet

--- turbustat/statistics/wavelets/wavelet_transform.py
+++ turbustat/statistics/wavelets/wavelet_transform.py
@@ -88,12 +88,13 @@ class Wavelet(BaseStatisticMixIn):
                              " Ensure that all lag values are larger than one "
                              "pixel.")
 
-        # Finally, limit the radius to a maximum of half the image size.
-        if np.any(pix_scal.value > min(self.data.shape) / 2.):
-            raise ValueError("At least one of the lags is larger than half of "
-                             "the image size (in the smallest dimension. "
-                             "Ensure that all lag values are smaller than "
-                             "this.")
+        # Catch floating point issues in comparing to half the image shape
+        half_comp = (np.floor(pix_scal.value) - min(self.data.shape) / 2.)
+
+        if np.any(half_comp > 1e-10):
+            raise ValueError("At least one of the lags is larger than half of"
+                             " the image size. Remove these lags from the "
+                             "array.")
 
         self._scales = values
 
