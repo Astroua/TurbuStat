57a743338a5e1aa6ea2249df2d3991fea2715014
e-koch <koch.eric.w@gmail.com>
2019-01-10 15:04:32 -0700

    Add scaling test script to examples

---
+++ Examples/scaling_test.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#SBATCH -t 12:00:00
+#SBATCH --mem=512000MB
+#SBATCH --ntasks=32
+#SBATCH --nodes=1
+#SBATCH --job-name=turbustat_scaling
+#SBATCH --output=turbustat_scaling-%J.out
+#SBATCH --error=turbustat_scaling-%J.err
+#SBATCH --account=def-eros-ab
+
+export OMP_NUM_THREADS=$SLURM_JOB_CPUS_PER_NODE
+
+module restore my_default
+
+# Use version loaded while building pyfftw
+module load fftw-mpi/3.3.6
+
+source /home/ekoch/.bashrc
+source /home/ekoch/preload.bash
+
+export scratch_path=/home/ekoch/scratch/turbustat_scaling
+export project_path=/home/ekoch/projects/rrg-eros-ab/ekoch/
+
+cd $scratch_path
+
+# Call script with number of cores
+$HOME/anaconda3/bin/python $HOME/code/turbustat/Examples/scaling_test.py
+
+# Copy the output files to the project path
+cp $scratch_path/*.txt $project_path/turbustat_scaling/
