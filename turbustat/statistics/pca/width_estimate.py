8b7f5c8d4898ab1e98d6e5203a4406ea0b53799d
e-koch <koch.eric.w@gmail.com>
2019-08-20 15:43:11 -0600

    And another

--- turbustat/statistics/pca/width_estimate.py
+++ turbustat/statistics/pca/width_estimate.py
@@ -133,10 +133,11 @@ def WidthEstimate2D(inList, method='contour', noise_ACF=0,
             output, cov = fit_2D_gaussian(xmat, ymat, z)
             try:
                 aspect = output.y_stddev_0.value[0] / output.x_stddev_0.value[0]
+                theta = output.theta_0.value[0]
             except IndexError:  # raised with astropy >v3.3 (current dev.)
                 aspect = output.y_stddev_0.value / output.x_stddev_0.value
+                theta = output.theta_0.value
 
-            theta = output.theta_0.value[0]
             rmat = ((xmat * np.cos(theta) + ymat * np.sin(theta))**2 +
                     (-xmat * np.sin(theta) + ymat * np.cos(theta))**2 *
                     aspect**2)**0.5
